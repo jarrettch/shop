fba7df4e9acc0742005f5d1cd07106feaa622192
jarrettch <jarrett.harris@gmail.com>
2014-04-24 18:21:46 -0700

    got orders to save to db

--- models/order.js
+++ models/order.js
@@ -1,3 +1,5 @@
+
+
 module.exports = function(sequelize, DataTypes) {
   var Order = sequelize.define('Order', {
     ordernumber: DataTypes.INTEGER
@@ -7,6 +9,15 @@ module.exports = function(sequelize, DataTypes) {
         Order.hasMany(models.Product)
       }
     }
+  })
+  
+  Order
+  .build({ ordernumber: 1 })
+  .save()
+  .success(function() {
+    console.log("success!")
+  }).error(function(error) {
+    throw error
   })
 
   return Order
--- routes/order.js
+++ routes/order.js
@@ -4,5 +4,13 @@
  */
 
 exports.list = function(req, res){
-  res.send("respond with a resource");
+  res.send( {
+    // orders: Order.findAll
+  });
 };
+
+exports.create = function(req, res) {
+  res.send( {
+    // orders: Order.create(attributes).success()
+  })
+}
